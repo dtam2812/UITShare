8ca17ebc77bf4e36ff2ad41f2a5f80e5f9da8646
thinh1311ss <pthinhn.com>
2026-03-11 21:17:29 +0700

    add FAQ page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import Admin from './pages/Admin/Admin.jsx'
-import Contact from './pages/Contact.jsx'
+import React, { useState, useEffect } from "react";
+import Admin from "./pages/Admin/Admin.jsx";
+import Contact from "./pages/Contact.jsx";
+import FAQ from "./pages/FAQ.jsx";
 
 function App() {
-        return (
-                <div>
-                        <Contact />
-                </div>
-        )
-        }
+  const [view, setView] = useState("faq");
+  useEffect(() => {window.scrollTo(0, 0);}, [view]);
 
+  if (view === "contact") {
+    return <Contact />;
+  }
 
-export default App
+  if (view === "faq") {
+    return <FAQ onNavigate={setView} />;
+  }
+
+  return <Admin />;
+}
+
+export default App;
