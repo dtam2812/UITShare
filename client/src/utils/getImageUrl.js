60bae9fa1fe76e3491e48de9bae7a63051e4e454
Tuong <tantuongdo0206@gmail.com>
2026-04-27 16:42:21 +0700

    fix url

--- client/src/utils/getImageUrl.js
+++ client/src/utils/getImageUrl.js
@@ -1,5 +1,8 @@
 export const getImageUrl = (url, fallback = "/default.jpg") => {
-  if (!url || url.includes("localhost") || url.startsWith("http://")) {
+  if (!url || url instanceof File || typeof url !== "string") {
+    return fallback;
+  }
+  if (url.includes("localhost") || url.startsWith("http://")) {
     return fallback;
   }
   return url;
